c9bd8204e0153084615099ac6efa8a016f667352
Parnol-06 <arnolferneyperez@hotmail.com>
2025-08-30 23:49:57 -0500

    Nuevo_Repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Desarrollo_Aplicaciones.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
